094bb350b24da544ab4bb58da9f2bd08f60d726f
Syed Aasim <aasimsyed398@gmail.com>
2026-07-15 15:09:28 +0530

    added search feature into Schema

--- src/DB/schema/products.ts
+++ src/DB/schema/products.ts
@@ -1,4 +1,4 @@
-import { varchar, uuid, pgTable, jsonb, uniqueIndex, integer } from 'drizzle-orm/pg-core';
+import { varchar, uuid, pgTable, jsonb, uniqueIndex, integer, text, index } from 'drizzle-orm/pg-core';
 import { user } from './user.js';
 import { sql } from 'drizzle-orm';
 
@@ -6,17 +6,25 @@ export const products = pgTable(
   'products',
   {
     id: uuid('id').primaryKey().notNull().defaultRandom(),
-    name: varchar('name', { length: 225 }).notNull(),
-    category: varchar('category', { length: 55 }).notNull(),
+    name: text('name').notNull(),
+    category: text('category').notNull(),
     userId: uuid('user_id').references(() => user.id, { onDelete: 'cascade' }),
     price: integer('price').notNull(),
     collageName: varchar('collageName', { length: 225 }),
-    detail: varchar('productDetail', { length: 225 }).notNull(),
+    detail: text('productDetail').notNull(),
     status: varchar('status', { length: 250 }).notNull(),
     images: jsonb('images')
       .notNull()
       .default(sql`'[]'::jsonb`),
   },
 
-  (table) => [uniqueIndex('product_title_unique').on(table.name, table.userId)],
+  (table) => [uniqueIndex('product_title_unique').on(table.name, table.userId),
+        index("products_search").using(
+      "gin",
+      sql`(setweight(to_tsvector('english', ${table.name}), 'A') ||
+          setweight(to_tsvector('english', ${table.category}), 'B') ||
+          setweight(to_tsvector('english', ${table.detail}), 'C')                                                       )`
+    )
+  ],
+  
 );
