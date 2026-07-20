e9eab5d2e7e74c5c5953c6df65087dc89a7b32f1
Syed Aasim <aasimsyed398@gmail.com>
2026-07-20 22:30:57 +0530

    started testing

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -2,13 +2,17 @@ import { ApiError } from '../utils/ApiError.js';
 import type { Request, Response, NextFunction, Errback } from 'express';
 
 const globalErrorHandler = (err: Errback, req: Request, res: Response, next: NextFunction) => {
+
   console.log(err);
 
   if (err instanceof ApiError) {
+
     return res.status(err.statusCode).json({ error: err.message });
+
   }
 
   return res.status(500).json({ error: 'Internal server error' });
+  
 };
 
 export { globalErrorHandler };
